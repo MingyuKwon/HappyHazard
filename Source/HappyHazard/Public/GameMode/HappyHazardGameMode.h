969c1751764fda8a51378bf2f42cd6a34d5ff8b5
Kwon Mingyu <kmgmg230601@gmail.com>
2024-08-14 19:46:04 +0900

    First, I reorganized the source file structure into the typical public and private structure.

---
+++ Source/HappyHazard/Public/GameMode/HappyHazardGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HappyHazardGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHappyHazardGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHappyHazardGameMode();
+};
+
+
+
